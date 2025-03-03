aa6e9a97ffd1da1034df39d556659c0b027dd153
sacivashi <razgabay123@gmail.com>
2025-03-03 15:39:41 +0200

    E

---
+++ game/pyrpg.py
@@ -0,0 +1,10 @@
+from game_data.data_inputs.input_name.input_name import Input_Name as name
+from game_data.data_inputs.input_role.input_role import Input_Role as role
+
+
+class PYRPG:
+	name.input_name()
+	role.input_role()
+	
+	
+PYRPG
--- game_data/data_inputs/input_name/input_name.py
+++ game_data/data_inputs/input_name/input_name.py
@@ -5,9 +5,9 @@ class Input_Name:
 	def input_name():
 		your_name = str(input(
 			"""Welcome to PyRPG.
-		This project is a text based input-output RPG game.
-		Please read #patch_notes file to see new patches or features.
-		To start you off, please input your name: """))
+This project is a text based input-output RPG game.
+Please read #Alpha I file to see new patches or features.
+To start you off, please input your name: """))
 		globals()["your_name"] = your_name
 		return your_name
 	
